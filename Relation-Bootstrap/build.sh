8679e179df1e8489d73fd03159a49716f42f7b74
Kelly M. T. Huffer <hufferkm@ornl.gov>
2016-07-11 10:27:56 -0400

    Updated pom.xml to fix Maven build issue with entity-extractor dependency.

--- Relation-Bootstrap/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mvn --non-recursive scm:checkout -Dmodule.name=entity-extractor -DscmVersion=bootstrap -DscmVersionType=branch
-cd entity-extractor
-mvn -e clean install -Dmaven.test.skip=true
-cd ..
-mvn -e clean package -Dmaven.test.skip=true
